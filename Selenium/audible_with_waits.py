5350ecee61e67921b5245d89ed2dda9983aa6ce8
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-09-13 19:33:47 +0300

    fix: removed firefox
    code runs on using chrome browser

--- Selenium/audible_with_waits.py
+++ Selenium/audible_with_waits.py
@@ -1,29 +1,28 @@
 from selenium import webdriver
-# Firefox
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-# Chrome
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import os
 import pandas as pd
 import time
 
 web = 'https://www.audible.com/adblbestsellers?ref=a_search_t1_navTop_pl0cg1c0r0&pf_rd_p=adc4b13b-d074-4e1c-ac46-9f54aa53072b&pf_rd_r=1F7DV0MPHV77Z61RX566'
 
-# Firefox
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-# Chrome
-# driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+options = ChromeOptions()
+# options.add_argument("--start-maximized")
+# options.add_experimental_option("detach", True) # to stay browser open
+options.add_argument("--headless=new") # to make browser invisible
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 driver.get(web)
 driver.maximize_window()
 
-pagination = driver.find_element_by_xpath('//ul[contains(@class, "pagingElements")]')
-pages = pagination.find_elements_by_tag_name('li')
+pagination = driver.find_element(By.XPATH, '//ul[contains(@class, "pagingElements")]')
+pages = pagination.find_elements(By.TAG_NAME, 'li')
 last_page = int(pages[-2].text)
 
 book_title = []
@@ -41,13 +40,13 @@ while current_page <= last_page:
     products = WebDriverWait(container, 5).until(EC.presence_of_all_elements_located((By.XPATH, './/li[contains(@class, "productListItem")]')))
 
     for product in products:
-        book_title.append(product.find_element_by_xpath('.//h3[contains(@class, "bc-heading")]').text)
-        book_author.append(product.find_element_by_xpath('.//li[contains(@class, "authorLabel")]').text)
-        book_length.append(product.find_element_by_xpath('.//li[contains(@class, "runtimeLabel")]').text)
+        book_title.append(product.find_element(By.XPATH, './/h3[contains(@class, "bc-heading")]').text)
+        book_author.append(product.find_element(By.XPATH, './/li[contains(@class, "authorLabel")]').text)
+        book_length.append(product.find_element(By.XPATH, './/li[contains(@class, "runtimeLabel")]').text)
 
     current_page = current_page + 1
     try:
-        next_page = driver.find_element_by_xpath('.//span[contains(@class , "nextButton")]')
+        next_page = driver.find_element(By.XPATH, './/span[contains(@class , "nextButton")]')
         next_page.click()
     except:
         pass
@@ -55,4 +54,7 @@ while current_page <= last_page:
 driver.quit()
 
 df_books = pd.DataFrame({'title': book_title, 'author': book_author, 'length': book_length})
-df_books.to_csv('audible_with_waits.csv', index=False)
+output_path = 'data_audible_with_waits'
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+df_books.to_csv(f'{output_path}/audible_with_waits.csv', index=False)
